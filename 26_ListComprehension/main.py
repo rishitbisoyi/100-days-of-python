b55c008e4a91357c476c513c30b8a23b40cd2133
rishitbisoyi <rishitbisoyi1234@gmail.com>
2026-06-01 16:34:42 +0530

    Day 26 List comprehension completed

---
+++ 26_ListComprehension/main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+data = pd.read_csv("26_ListComprehension/nato_phonetic_alphabet.csv")
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+word = input("Enter a word: ").upper()
+output_list = [phonetic_dict[letter] for letter in word]
+print(output_list)
